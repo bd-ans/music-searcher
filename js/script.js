ed6333b898f037cab3e53756aa862c3545e54403
Elayt <84461884+bd-ans@users.noreply.github.com>
2022-09-13 22:48:41 +0500

    .

--- js/script.js
+++ js/script.js
@@ -1,60 +1,59 @@
-const countryList = $('.js-country-list');
-const moviesCardTemplate = $('#template-element').content;
+const musicsList = $('.js-music-list');
+const musicsCardTemplate = $('#template-element').content;
 
-const searchInput = $('.js-search-input');
-const searchSelect = $('.js-search-select');
-const searchBtn = $('.js-search-btn');
+const elSearchInput = $('.js-search-input');
+const elSearchBtn = $('.js-search-btn');
 const elFailTxt = $('.js-fail-txt');
 
 let arr = [];
 
 // render function
 function mainFunc() {
-    // creating elements for movies list
-    let createMovieElement = function (arr) {
-        let movieElement = moviesCardTemplate.cloneNode(true);
-        movieElement.querySelector('.js-movie-img').src = `https://api.napster.com/imageserver/v2/albums/${arr.albumId}/images/300x300.jpg`;
-        movieElement.querySelector('.js-movie-img').alt = arr.name;
-        movieElement.querySelector('.js-modal-movie-img').src = `https://api.napster.com/imageserver/v2/albums/${arr.albumId}/images/300x300.jpg`;
-        movieElement.querySelector('.js-modal-movie-img').alt = arr.name;
+    // creating elements for music list
+    let createMusicElements = function (arr) {
+        let musicElement = musicsCardTemplate.cloneNode(true);
+        musicElement.querySelector('.js-music-img').src = `https://api.napster.com/imageserver/v2/albums/${arr.albumId}/images/300x300.jpg`;
+        musicElement.querySelector('.js-music-img').alt = arr.name;
+        musicElement.querySelector('.js-modal-music-img').src = `https://api.napster.com/imageserver/v2/albums/${arr.albumId}/images/300x300.jpg`;
+        musicElement.querySelector('.js-modal-music-img').alt = arr.name;
 
-        movieElement.querySelector('.js-country-title').textContent = arr.name;
-        movieElement.querySelector('.js-modal-title').textContent = arr.name;
-        movieElement.querySelector('.js-country-capital').textContent = arr.artistName;
-        movieElement.querySelector('.js-country-population').textContent = arr.albumName;
+        musicElement.querySelector('.js-music-title').textContent = arr.name;
+        musicElement.querySelector('.js-modal-title').textContent = arr.name;
+        musicElement.querySelector('.js-music-artist-name').textContent = arr.artistName;
+        musicElement.querySelector('.js-music-album-name').textContent = arr.albumName;
 
-        movieElement.querySelector('.audio').src = arr.previewURL;
-        movieElement.querySelector('.js-country-independent').textContent = arr.disc;
-        movieElement.querySelector('.js-country-borders').textContent = arr.playbackSeconds;
-        movieElement.querySelector('.js-country-timezones').textContent = arr.shortcut;
-        movieElement.querySelector('.js-music-explicits').textContent = arr.isExplicit;
-        movieElement.querySelector('.js-music-hires').textContent = arr.isAvailableInHiRes;
+        musicElement.querySelector('.audio').src = arr.previewURL;
+        musicElement.querySelector('.js-music-disc').textContent = arr.disc;
+        musicElement.querySelector('.js-music-playback').textContent = arr.playbackSeconds;
+        musicElement.querySelector('.js-music-shortcuts').textContent = arr.shortcut;
+        musicElement.querySelector('.js-music-explicits').textContent = arr.isExplicit;
+        musicElement.querySelector('.js-music-hires').textContent = arr.isAvailableInHiRes;
 
         let formats = [];
         arr.formats.forEach((item, i) => {
             formats.push(item.name);
         });
-        movieElement.querySelector('.js-country-start-week').textContent = formats.join(', ');
-        movieElement.querySelector('.js-modal').id = `exampleModal${arr.index}`;
-        movieElement.querySelector('.js-modal-title').id = `exampleModal${arr.index}`;
-        movieElement.querySelector('.js-modal-btn').setAttribute('data-bs-target', `#exampleModal${arr.index}`);
+        musicElement.querySelector('.js-music-formats').textContent = formats.join(', ');
+        musicElement.querySelector('.js-modal').id = `exampleModal${arr.index}`;
+        musicElement.querySelector('.js-modal-title').id = `exampleModal${arr.index}`;
+        musicElement.querySelector('.js-modal-btn').setAttribute('data-bs-target', `#exampleModal${arr.index}`);
 
-        return movieElement;
+        return musicElement;
     }
 
     // render function
-    let renderCountries = function (arr) {
-        countryList.innerHTML = null;
+    let renderMusics = function (arr) {
+        musicsList.innerHTML = null;
         let fragment = document.createDocumentFragment();
     
-        arr.forEach(movie => {
-            fragment.appendChild(createMovieElement(movie));
+        arr.forEach(music => {
+            fragment.appendChild(createMusicElements(music));
         });
         
-        countryList.appendChild(fragment);
+        musicsList.appendChild(fragment);
     }
     
-    renderCountries(arr);
+    renderMusics(arr);
     if (arr.length === 0) {
         elFailTxt.classList.remove('d-none');
     } else {
@@ -63,7 +62,7 @@ function mainFunc() {
 }
 
 // api request
-const searchMovies = async music => {
+const searchMusics = async music => {
     try {
         const urlApi = await fetch(`https://api.napster.com/v2.2/search?apikey=YTkxZTRhNzAtODdlNy00ZjMzLTg0MWItOTc0NmZmNjU4Yzk4&query=${music}&type=track`);
         const data = await urlApi.json();
@@ -73,7 +72,7 @@ const searchMovies = async music => {
             elFailTxt.classList.remove('d-none');
             return;
         } else {
-            searchInput.blur();
+            elSearchInput.blur();
             elFailTxt.classList.add('d-none');
             arr = data.search.data.tracks;
             mainFunc();
@@ -81,33 +80,33 @@ const searchMovies = async music => {
     } catch (err) {
         console.log(err);
     } finally {
-        searchInput.value = '';
-            searchBtn.disabled = false;
-            searchBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+        elSearchInput.value = '';
+            elSearchBtn.disabled = false;
+            elSearchBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
             </svg>`;
     }
 }
 
 // Search input enter
-searchInput.addEventListener('keyup', function (event) {
+elSearchInput.addEventListener('keyup', function (event) {
     if (event.key === 'Enter') {
-        searchBtn.click();
+        elSearchBtn.click();
     }
 });
 
 // Search btn click
-searchBtn.onclick = function () {
-    let value = searchInput.value.toLowerCase().trim();
+elSearchBtn.onclick = function () {
+    let value = elSearchInput.value.toLowerCase().trim();
     if (value === '') {
-        searchInput.value = null;
+        elSearchInput.value = null;
         return;
     } else {
-    countryList.innerHTML = null;
+    musicsList.innerHTML = null;
     arr = [];
-    searchBtn.disabled = true;
-    searchBtn.innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`;
+    elSearchBtn.disabled = true;
+    elSearchBtn.innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`;
 
-    searchMovies(value)
+    searchMusics(value)
     }
 }
